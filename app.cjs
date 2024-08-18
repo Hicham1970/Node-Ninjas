d68d0d14c2c62be4f0001c84dab90ddb52e5adc3
Hicham1970 <hicham.garroum@sgs.com>
2024-08-18 14:17:35 +0100

    add contact.ejs

--- app.cjs
+++ app.cjs
@@ -1,27 +1,48 @@
 const express = require("express");
 const path = require("path");
+const { title } = require("process");
 
 //Express app
 const app = express();
+// Register view engin : moteur de vues
+app.set("view engine", "ejs");
+// Set the path to the views folder
+app.set("views", path.join(__dirname, "views"));
 
 //listen for request
 app.listen(3000);
 
 //GET request
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.render(path.join(__dirname, "views", "index"), { title: "Home" });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "about.html"));
+  res.render(path.join(__dirname, "views", "about"), { title: "About" });
 });
+// Create
+app.get("/blogs/create", (req, res) => {
+  res.render(path.join(__dirname, "views", "create"), { title: "Create" });
+});
+// test
+app.get("/contact", (req, res) => {
+  let d = new Date();
+  let slt = d.getHours() < 17 ? "Bonjour" : "Bonsoir";
 
-// redirect
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+  let obj = {
+    name: ["A", "B", "C", "D", "E"],
+    salutation: slt,
+  };
+
+  res.render(path.join(__dirname, "views", "contact"), {
+    title: "Contact",
+    obj: obj,
+  });
 });
 
 //404 Page :
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res
+    .status(404)
+    .render(path.join(__dirname, "views", "404"), { title: "404" });
 });

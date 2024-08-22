232d59b7081abb352d392f3b170d5594633fdf17
Hicham1970 <hicham.garroum@sgs.com>
2024-08-22 18:49:50 +0100

    Middleware and static middleware de express, pour integration du style.css

--- app.cjs
+++ app.cjs
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const { title } = require("process");
+const morgan = require("morgan");
 
 //Express app
 const app = express();
@@ -11,6 +12,22 @@ app.set("views", path.join(__dirname, "views"));
 
 //listen for request
 app.listen(3000);
+
+// Creation de mon propre middleware:
+// app.use(function (req, res, next) {
+//   console.log("Une nouvelle requête est crée !! ");
+//   console.log("Host :", req.hostname);
+//   console.log("Path :", req.path);
+//   console.log("Method :", req.method);
+//   next();
+// });
+
+// Middleware & static files comme photos ou styles.css ...
+app.use(express.static(path.join(__dirname, "public")));
+
+// Utiliser le middleware Morgan:
+// app.use(morgan("dev"));
+app.use(morgan("tiny"));
 
 //GET request
 app.get("/", (req, res) => {
@@ -55,6 +72,11 @@ app.get("/contact", (req, res) => {
     title: "Contact",
     obj: obj,
   });
+});
+// 2 ème middleware
+app.use((req, res, next) => {
+  console.log("Je suis le middleware 2 !! ");
+  next();
 });
 
 //404 Page :

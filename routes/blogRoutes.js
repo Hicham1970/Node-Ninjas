87d0659359c29c6539c4dbcfdbf4b12510ba908a
Hicham1970 <hicham.garroum@sgs.com>
2024-09-12 00:49:10 +0100

    Add an svg for delete icon

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,67 +1,16 @@
 const express = require("express");
-const Blog = require("../Models/blog");
+const blogController = require("../controllers/blogController");
 
-// create a new instance of a router object
 const router = express.Router();
 
-// rattacher les gestionnaires de request au routeur (a la place de app)
-// router est comme une mini app
-router.get("/", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", {
-        title: "All Blogs",
-        blogs: result,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/", blogController.blog_index);
 
-// Post Request:
-router.post("/", (req, res) => {
-  const blog = new Blog(req.body);
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.post("/", blogController.blog_create_post);
 
-// create:
-router.get("/create", (req, res) => {
-  res.render("create", {
-    title: "Create a New Blog",
-  });
-});
+router.get("/create", blogController.blog_create_get);
 
-// Find By Id:
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/:id", blogController.blog_details);
 
-// Delete request:
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-// exporte le routeur
+router.delete("/:id", blogController.blog_delete);
+
 module.exports = router;
